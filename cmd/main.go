f30fb806e373e51b358124c159b74b9e3c0548a7
viktorov13 <viktorov.evgeniy2222@gmail.com>
2026-06-27 00:16:35 +0300

    base struct, endpoints

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,30 @@ package main
 import (
 	"log"
 
+	"github.com/spf13/viper"
 	todo_app "github.com/viktorov13/todo-app"
 	"github.com/viktorov13/todo-app/pkg/handler"
+	"github.com/viktorov13/todo-app/pkg/repository"
+	"github.com/viktorov13/todo-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := InitConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo_app.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
